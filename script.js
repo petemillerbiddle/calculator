64176b958f0aed389d9cd8c20ffaff8c5f2fbeeb
petemillerbiddle <petemillerbiddle@gmail.com>
2022-06-14 09:20:46 -0400

    Create fxn operate() to call arith. fxns, values

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b){
+    if (operator == '+') return add(a, b);
+    else if (operator == '-') return subtract(a, b);
+    else if (operator == '*') return multiply(a, b);
+    else if (operator == '/') return divide(a, b);
+}
