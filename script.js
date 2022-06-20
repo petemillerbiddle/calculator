966988becc1df0574988248e3b1e8e14b99eac06
petemillerbiddle <petemillerbiddle@gmail.com>
2022-06-20 11:07:04 -0400

    Update display with 7 & 8 buttons

--- script.js
+++ script.js
@@ -14,9 +14,29 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b){
+function operate(operator, a, b) {
     if (operator == '+') return add(a, b);
     else if (operator == '-') return subtract(a, b);
     else if (operator == '*') return multiply(a, b);
     else if (operator == '/') return divide(a, b);
+}
+
+let displayValue = 15;
+const display = document.querySelector('#display');
+display.textContent = displayValue;
+
+const digitButtons = document.querySelectorAll('.digit-btn');
+digitButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        updateDisplay(button.id);
+    });
+});
+
+
+function updateDisplay(digit) {
+    //TODO
+    //update displayValue with button pressed
+    //update display with new displayValue   
+    displayValue += digit;
+    display.textContent = displayValue;
 }
